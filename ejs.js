7e5c972347f7776f9ae8aef32eedc580b472bd76
Gokul-24-2001 <gokulashok1002@gmail.com>
2025-10-22 19:08:01 +0530

    Add files via upload

---
+++ ejs.js
@@ -0,0 +1,4 @@
+<script>function fun() {  
+       var x = document.getElementById("key").value;  
+        alert("Hi"+"x"+ "you have successfully submitted the details");  
+    }  </script>
---
+++ project_login_details.js
@@ -0,0 +1,37 @@
+<doctype html>
+<html>
+<head><b> LOGIN_DETAILS</b></head>
+<br>
+<body><br>
+
+<form>
+	<div style="color:Green";>
+	<label > student name:</label>
+	  <input type="text" name="username" placeholder="enter your name"/>
+	  <br>
+	  	Email address:
+		<input type="email" name="email_address"/>
+<br>
+		<label > password:</label>
+	  <input type="password" id="pwd"placeholder="enter your password" /><br>
+	  <br>
+	  <label> confirm password:</label>
+	  	  <input type="password" id="pwd"placeholder="enter your confirm password" /><br>
+		  
+		  course name:
+		  <select >
+		  <option id="course"> python</option>
+		  <option id="course"> java</option>
+		  <option id="course"> db</option>
+		  <option id="course"> sql</option>
+		  </select><br>
+		  Gender:
+		  <input type="radio"  >male</input>
+		  <input type="radio"> female</input>
+		  <input type="radio"> others</input><br>
+		  </div>
+		  
+
+	 -->
+	 </body>
+	 </html>
